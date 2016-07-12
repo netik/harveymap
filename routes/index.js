612f2ad0449c262e02524430757b40c7b1ed58a7
John Adams <jna@retina.net>
2016-07-12 16:45:52 -0700

    add fbapi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'HarveyMap', apikey: process.env.GMAPI_KEY });
+	res.render('index', { 
+		    title: 'HarveyMap', 
+		    apikey: process.env.GMAPI_KEY,
+		    fbase_apikey: process.env.FBASEAPI_KEY
+		   });
 });
 
 module.exports = router;
